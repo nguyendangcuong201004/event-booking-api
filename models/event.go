782db79e5e2734dcf4b2ab10c27bf9b7da48bcf1
Nguyen Dang Cuong <dangcuong201004@gmail.com>
2026-07-06 01:45:55 +0700

    feat: create api getEvents, createEvent and api-test

---
+++ models/event.go
@@ -0,0 +1,24 @@
+package models
+
+import "time"
+
+type Event struct {
+	ID          int
+	Name        string    `binding:"required"`
+	Description string    `binding:"required"`
+	Location    string    `binding:"required"`
+	DateTime    time.Time `binding:"required"`
+	UserID      int
+}
+
+var events = []Event{}
+
+func (e Event) Save() {
+	// Add to database
+	events = append(events, e)
+
+}
+
+func GetAllEvents() []Event {
+	return events
+}
